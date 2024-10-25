988bbe5703c1e359a423c1b236476d1316c3ff21
Gaurav Gorane <gauravvg11@gmail.com>
2024-10-25 17:38:57 +0530

    change: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, HashRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import InfoSite from "./Pages/Infosite/Info-site";
 import Airbnb from "./Pages/Airbnb/Airbnb";
@@ -9,14 +9,14 @@ import Tenzies from "./Pages/Tenzies/Tenzies";
 
 function Components() {
   return (
-    <HashRouter>
+    <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/info-site" element={<InfoSite />} />
       <Route path="/airbnb" element={<Airbnb />} />
       <Route path="/meme-generator" element={<MemePage />} />
       <Route path="/notes-app" element={<NoteApp />} />
       <Route path="/tenzies" element={<Tenzies />} />
-    </HashRouter>
+    </Routes>
   );
 }
 
